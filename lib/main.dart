3a48e7eb16f04bd00a07589efd028cf4beceb73c
t-bolt <isholasobuur@gmail.com>
2023-11-22 11:10:38 +0100

    resolve fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shoppa/splash_screen.dart';
 import 'package:shoppa/style/color.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(const MyApp());
 }
 
